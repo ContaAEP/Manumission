596318b5646801e5d365490edea05e4c1be6ff24
Jean Koval <jeancarloskoval2002@gmail.com>
2021-06-10 17:57:40 -0300

    Demo

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 
-$template = file_get_contents('/html/index.html');
+$app->get('/cowsay', function() use($app) {
+    $app['monolog']->addDebug('cowsay');
+    return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+  });
 
-echo $template;
+// $template = file_get_contents("index.html");
+
+// echo $template;
 
 
